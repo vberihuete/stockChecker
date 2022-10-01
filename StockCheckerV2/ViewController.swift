484e17c8d49dbbc2e8e061d96c59585b5b8364a8
Vincent Berihuete <vberihuete@gmail.com>
2022-10-01 17:22:52 +0100

    Initial Commit

---
+++ StockCheckerV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockCheckerV2
+//
+//  Created by Vincent Berihuete Paulino on 01/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
